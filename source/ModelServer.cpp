7cfc2344a84fdccd4f5e4d62b3c624a357367c61
鈴木希海 <121pg0014@amg.tokyo.ac.jp>
2022-01-19 12:09:07 +0900

    3Dモデルを管理するモデルサーバを仮実装中

---
+++ source/ModelServer.cpp
@@ -0,0 +1,93 @@
+#include "ModelServer.h"
+#include <DxLib.h>
+#include "ApplicationMain.h"
+
+namespace Gyro {
+  namespace Model {
+
+    ModelServer::ModelServer(Application::ApplicationMain& app) :
+      AppServer::ServerTemplateUnordered<std::string, std::vector<int>>(),
+      _app(app)
+    {
+      _animations.clear();
+    }
+
+    bool ModelServer::Release() {
+      // 読み込まれている全てのモデルデータとアニメーションを削除する
+      for (auto&& [key, handles] : _registry) {
+        // モデルハンドルの削除
+        DeleteHandles(handles);
+      }
+      _directry.clear();   // ディレクトリパスを削除
+      _animations.clear(); // アニメーションの削除
+      return true;
+    }
+
+    bool ModelServer::SetDirectryPath(std::string_view directry) {
+      namespace fs = std::filesystem;
+      // ディレクトリパスは設定されていないか
+      if (!_directry.empty()) {
+        return false; // 既に登録されている
+      }
+      // 対象ディレクトリは存在するか
+      if (!fs::is_directory(directry.data())) {
+        return false; // 存在しない
+      }
+      _directry = directry; // ディレクトパスを登録する
+      return true;
+    }
+
+    bool ModelServer::LoadModel(std::string_view fileName) {
+      // ファイルは存在するか
+      if (!PathExist(fileName.data())) {
+        return false; // 存在しない
+      }
+      // キーが重複してるかの判定
+      if (_registry.contains(fileName.data())) {
+        // 登録されている場合は既存データを削除する
+        auto&& handles = _registry.at(fileName.data());
+        DeleteHandles(handles);    // モデルハンドルを削除
+        _registry.erase(fileName.data()); // 既存の要素を削除
+        _animations.erase(fileName.data()); // アニメーションも削除
+      }
+      // モデルハンドルの読み取り
+      auto handle = ModelServer::MV1LoadModel(fileName.data());
+      std::vector<int> handles(handle);
+      // アニメーション総数の取得
+      auto animNum = MV1GetAnimNum(handle);
+      for (auto no = 0; no < animNum; ++no) {
+        // アニメーション名の取得
+        auto animName = MV1GetAnimName(handle, no);
+        // アニメーション名とアニメ番号を登録
+        _animations.emplace(animName, no);
+      }
+      return true;
+    }
+
+    bool ModelServer::PathExist(std::filesystem::path path) {
+#ifndef _DEBUG
+      return std::filesystem::exists(_directry / path);
+#else
+      // 対象ファイルは存在するか
+      if (!std::filesystem::exists(_directry / path)) {
+        // 存在しない場合はログに出力
+        DebugString(LogicError((_directry / path).string() + ":は存在しません").what());
+        return true;
+      }
+      return true; // 存在する
+#endif
+    }
+
+    void ModelServer::DeleteHandles(std::vector<int>& handles) {
+      // 全モデルハンドルの削除を行う
+      for (auto&& handle : handles) {
+        MV1DeleteModel(handle);
+      }
+      handles.clear(); // コンテナを削除
+    }
+
+    int ModelServer::MV1LoadModel(std::filesystem::path path) const {
+      return DxLib::MV1LoadModel((_directry / path).string().c_str());
+    }
+  } // namespace Model
+} // namespace Gyro
---
+++ source/ModelServer.h
@@ -0,0 +1,75 @@
+#pragma once
+#include <appframe.h>
+
+/**
+ * @brief ゲームベース
+ */
+namespace Gyro {
+  /**
+   * @brief アプリケーションベース
+   */
+  namespace Application {
+    class ApplicationMain;
+  } // namespace Application
+  /**
+   * @brief モデルベース
+   */
+  namespace Model {
+    /**
+     * @brief 名前空間の別名定義
+     */
+    namespace AppServer = AppFrame::Server;
+    /**
+     * @class ModelServer
+     * @brief モデルデータとアニメーションを管理するサーバクラス
+     */
+    class ModelServer : public AppServer::ServerTemplateUnordered<std::string, std::vector<int>> {
+    public:
+      /**
+       * @brief コンストラクタ
+       * @param app アプリケーションメインの参照
+       */
+      ModelServer(Application::ApplicationMain& app);
+      /**
+       * @brief  リソースの解放処理
+       * @return 
+       */
+      bool Release() override;
+      /**
+       * @brief  モデルファイルが格納されているディレクトリパスの設定
+       * @param  directry ディレクトリパス
+       * @return true:設定完了 false:失敗
+       */
+      bool SetDirectryPath(std::string_view directry);
+      /**
+       * @brief  モデルハンドルの読み取り
+       * @param  filePath 対象モデルのファイルパス
+       * @return true:読み取り成功 false:読み取り失敗
+       */
+      bool LoadModel(std::string_view fileName);
+    private:
+      Application::ApplicationMain& _app; //!< アプリケーションメインの参照
+      //!< モーションデータをファイル名(文字列)で管理する連想配列
+      std::unordered_map<std::string, int> _animations{};
+      std::filesystem::path _directry{}; //!< ディレクトリパス
+      /**
+       * @brief  パスが有効かの判定
+       * @param  path 判定パス
+       * @return true:有効 false:有効ではない
+       */
+      bool PathExist(std::filesystem::path path);
+      /**
+       * @brief モデルハンドルの削除を行う
+       * @param handles モデルハンドルが格納されたコンテナの参照
+       */
+      void DeleteHandles(std::vector<int>& handles);
+      /**
+       * @brief  Mv1モデルの読み取り
+       * @param  path ファイル名
+       * @return モデルハンドル
+       *         読み取りに失敗した場合は-1を返す
+       */
+      int MV1LoadModel(std::filesystem::path path) const;
+    };
+  } // namespace Model
+} // namespace Gyro
