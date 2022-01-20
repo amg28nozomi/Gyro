f46a1d058beb3d9aa44b6ae15f2a7bba7a4d9bb1
鈴木希海 <121pg0014@amg.tokyo.ac.jp>
2022-01-20 12:25:44 +0900

    当たり判定の基底クラスCollisionBaseを追加

---
+++ source/CollisionBase.h
@@ -0,0 +1,45 @@
+/*****************************************************************//**
+ * @file   CollisionBase.h
+ * @brief  当たり判定用情報の基底クラス
+ * 
+ * @author 鈴木希海
+ * @date   January 2022
+ *********************************************************************/
+#pragma once
+
+namespace AppFrame::Math {
+  class Vector4;
+}
+
+ /**
+ * @brief ゲームベース
+ */
+namespace Gyro {
+  /**
+   * @brief オブジェクトベース
+   */
+  namespace Object {
+    namespace AppMath = AppFrame::Math;
+    /**
+     * @class CollisionBase
+     * @brief 当たり判定情報
+     */
+    class CollisionBase {
+    public:
+      /**
+       * @brief 当たり判定の基底クラス
+       */
+      CollisionBase();
+      /**
+       * @brief 当たり判定の更新
+       */
+      virtual void Process();
+      /**
+       * @brief 当たり判定の描画
+       */
+      virtual void Draw();
+    protected:
+    };
+  } // namespace Object
+} // namespace Gyro
+
