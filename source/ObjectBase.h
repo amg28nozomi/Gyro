e727b90c46257aa40a32336a714ca3bb77491949
鈴木希海 <121pg0014@amg.tokyo.ac.jp>
2022-01-24 01:22:16 +0900

    ObjectBaseクラス：識別番号と状態を表す列挙型クラスを追加

--- source/ObjectBase.h
+++ source/ObjectBase.h
@@ -7,7 +7,6 @@
  *********************************************************************/
 #pragma once
 #include "appframe.h"
-
 /**
  * @brief ゲームベース
  */
@@ -29,6 +28,22 @@ namespace Gyro {
     class ObjectBase {
     public:
       /**
+       * @brief オブジェクト識別番号の列挙型クラス
+       */
+      enum class ObjectId {
+        Object, //!< オブジェクト
+        Player, //!< プレイヤー
+        Enemy   //!< 敵
+      };
+      /**
+       * @brief オブジェクトの状態を表す列挙型クラス
+       */
+      enum class ObjectState {
+        Active, //!< 活動状態
+        Paused, //!< 停止状態
+        Dead    //!< 死亡状態
+      };
+      /**
        * @brief コンストラクタ
        * @param app アプリケーションの参照
        */
@@ -48,8 +63,24 @@ namespace Gyro {
        * @return true
        */
       virtual bool Draw() const;
+      /**
+       * @brief  識別番号の取得
+       * @return 識別番号
+       */
+      ObjectId GetId() const {
+        return _id;
+      }
+      /**
+       * @brief  状態の取得
+       * @return 状態
+       */
+      ObjectState GetState() const {
+        return _state;
+      }
     protected:
       Application::ApplicationMain& _app; //!< アプリケーションの参照
+      ObjectId _id{ObjectId::Object}; //!< オブジェクトの識別番号
+      ObjectState _state{ObjectState::Active}; //!< 状態
       AppFrame::Math::Vector4 _position;  //!< 座標行列
       AppFrame::Math::Vector4 _rotation;  //!< 回転行列
       bool _gravity; //!< 重力処理を行うか(true:重力処理を行う false:重力処理を行わない)
