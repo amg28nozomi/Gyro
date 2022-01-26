d52b82f04bb9be1528adbe0818392dd25304abb3
121pg0019 <121pg0019@tokyo.amg.ac.jp>
2022-01-27 03:52:38 +0900

    プレイヤークラス：プレイヤークラスのいらないコメントを削除

--- source/Player.h
+++ source/Player.h
@@ -17,21 +17,6 @@ namespace AppFrame::Math{}
  */
 namespace Gyro {
   /**
-   * @brief カメラベース
-   */
-  namespace Camera {
-    /**
-     * @class Camera
-     * @brief カメラクラス
-     */
-    /*class Camera {
-    public:
-      VECTOR _pos;
-      VECTOR _target;
-      float _clipNear, _clipFar;
-    };*/
-  } // namespace Camera
-  /**
    * @brief プレイヤーベース
    */
   namespace Player {
@@ -147,90 +132,5 @@ namespace Gyro {
 #endif
     };
   }
-    /**
-     * @brief プレイヤー
-     */
-    //namespace Player {
-    //    /**
-    //     * @class Player
-    //     * @brief オブジェクトベースのサブクラス
-    //     */
-    //    class Player : public Object::ObjectBase {
-    //    public:
-    //      /**
-    //       * @brief コンストラクタ
-    //       * @param app アプリケーションの参照
-    //       */
-    //      Player(Application::ApplicationMain& app);
-    //      /**
-    //       * @brief  初期化処理
-    //       * @return true
-    //       */
-    //      bool Init() override;
-    //      /**
-    //       * @brief  更新処理
-    //       * @return true
-    //       */
-    //      bool Process() override;
-    //      /**
-    //       * @brief  描画処理
-    //       * @return true
-    //       */
-    //      bool Draw() const override;
-
-
-    //        /**
-    //         * @enum class
-    //         */
-    //        enum class STATE {
-    //            IDLE,
-    //            WALK,
-    //            RUN,
-    //            ATTACK,
-    //            JUMP,
-    //        };
-    //        /**
-    //         * @brief コンストラクタ
-    //         */
-    //        Player(Application::ApplicationMain& app);
-    //        //Player();
-    //        /**
-    //         * @brief  初期化処理
-    //         * @return true
-    //         */
-    //        bool Init() override;
-    //        /**
-    //         * @brief  更新処理
-    //         * @return true
-    //         */
-    //        bool Process() override;
-    //        /**
-    //         * @brief  描画処理
-    //         * @return true
-    //         */
-    //        bool Draw() const override;
-    //    protected:
-    //        int _handleMap;
-    //        int _frameMapCollision;
-    //        int _handleModel;       //!< モデルハンドル
-    //        int _handleSkySphere;   //!< スカイスフィアハンドル
-    //        int _attachIndex;       //!< アニメーションのアタッチ
-    //        float _totalTime;       //!< 総再生時間
-    //        float _playTime;        //!< 
-    //        VECTOR _position;       //!< 座標
-    //        VECTOR _dir;            //!< 向き
-
-    //        STATE _state;           //!< 状態
-
-    //        Camera::Camera _cam;
-    //        /**
-    //         * @brief リソースの読み取り処理
-    //         */
-    //        void LoadResource();
-    //        /**
-    //         * @brief カメラの設定
-    //         */
-    //        void SetCamera();
-    //    };
     //} // namespace Player
 } // namespace Gyro
