ddc5e06fa62482fb1e36a2d1a979b62189be08ad
鈴木希海 <121pg0014@amg.tokyo.ac.jp>
2022-02-11 00:24:15 +0900

    追加

---
+++ source/CollisionComponent.cpp
@@ -0,0 +1,16 @@
+/*****************************************************************//**
+ * @file   CollisionComponent.cpp
+ * @brief  衝突判定処理を行うコリジョンコンポーネントクラスの定義
+ * 
+ * @author 鈴木希海
+ * @date   February 2022
+ *********************************************************************/
+#include "CollisionComponent.h"
+
+namespace Gyro {
+  namespace Object {
+    
+    CollisionComponent::CollisionComponent() {
+    }
+  } // namespace Object
+} // namespace Gyro
