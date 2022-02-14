be97fd60f0f929ddc5da1d670bc25cd264120daa
amg121pg0023 <121pg0023@gmail.com>
2022-02-14 22:19:06 +0900

    ApplicationMainにエフェクト周りの実態を生成

--- source/ApplicationMain.cpp
+++ source/ApplicationMain.cpp
@@ -1,7 +1,7 @@
 /*****************************************************************//**
  * @file   ApplicationMain.cpp
  * @brief  アプリケーションベースのサブクラスの定義
- * 
+ *
  * @author 鈴木希海
  * @date   January 2022
  *********************************************************************/
@@ -13,75 +13,79 @@
 #include "SpawnServer.h"
 
 namespace Gyro {
-  namespace Application {
-    // アプリケーションの実体
-    ApplicationMain application;
-    
-    ApplicationMain::ApplicationMain() : ApplicationBase() {
-      // 実体の登録を行うか
-      if (SetInstance()) {
-        // 実体作成
-        _instance = std::make_shared<ApplicationMain>();
-      }
-    }
+    namespace Application {
+        // アプリケーションの実体
+        ApplicationMain application;
 
-    bool ApplicationMain::Init() {
-      if (!ApplicationBase::Init()) {
-        return false; // 初期化失敗
-      }
-      // Effekseerの設定
-      if (!Effekseer()) {
-          return false; // 設定失敗
-      }
-      // オブジェクトサーバの生成
-      _objectServer = std::make_unique<Object::ObjectServer>();
-      _objectServer->Init();
-      // スポーンサーバの生成
-      _spawnServer = std::make_unique<Object::SpawnServer>(*this);
-      _spawnServer->Init();
-      // カメラの生成
-      _camera = std::make_unique<Camera::Camera>();
-      _camera->Init();
-      // モードゲームの登録
-      _modeServer->AddMode("Game", std::make_shared<Mode::ModeGame>(*this));
-      _modeServer->TransionToMode("Game");
-      return true;
-    }
-
-    bool ApplicationMain::Input() {
-      auto flag = ApplicationBase::Input();
-      // 入力状態の取得
-      //auto state = _input->GetJoypadState();
-      //// ゲームを終了するかの判定
-      //if (state.GetKey() & PAD_INPUT_1) {
-      //  _state = State::Quit;
-      //  return true;
-      //}
-      return flag;
-    }
-
-    void ApplicationMain::Terminate() {
-        // Effekseerを終了する。
-        Effkseer_End();
-    }
-
-    bool ApplicationMain::Process() {
-      return ApplicationBase::Process();
-    }
-
-    bool ApplicationMain::Effekseer() {
-        //DirectX11を使用
-        SetUseDirect3DVersion(DX_DIRECT3D_11);
-        //Effekseerの初期化
-        //引数には画面に表示する最大パーティクル数を設定
-        if (Effekseer_Init(8000) == -1) {
-            return false;
+        ApplicationMain::ApplicationMain() : ApplicationBase() {
+            // 実体の登録を行うか
+            if (SetInstance()) {
+                // 実体作成
+                _instance = std::make_shared<ApplicationMain>();
+            }
         }
-        //フルスクリーンウインドウの切り替えでリソースが消えるのを防ぐ
-        SetChangeScreenModeGraphicsSystemResetFlag(FALSE);
-        //DXライブラリのデバイスロストした時のコールバックを設定
-        Effekseer_SetGraphicsDeviceLostCallbackFunctions();
-        return true;
-    }
-  } // namespace Application
+
+        bool ApplicationMain::Init() {
+            if (!ApplicationBase::Init()) {
+                return false; // 初期化失敗
+            }
+            // Effekseerの設定
+            if (!Effekseer()) {
+                return false; // 設定失敗
+            }
+            // オブジェクトサーバの生成
+            _objectServer = std::make_unique<Object::ObjectServer>();
+            _objectServer->Init();
+            // スポーンサーバの生成
+            _spawnServer = std::make_unique<Object::SpawnServer>(*this);
+            _spawnServer->Init();
+            // カメラの生成
+            _camera = std::make_unique<Camera::Camera>();
+            _camera->Init();
+            // エフェクトサーバの生成
+            _effectServer = std::make_unique<Effect::EffectServer>();
+            // エフェクトの生成
+            _effect = std::make_unique<Effect::EffectComponent>(*this);
+            // モードゲームの登録
+            _modeServer->AddMode("Game", std::make_shared<Mode::ModeGame>(*this));
+            _modeServer->TransionToMode("Game");
+            return true;
+        }
+
+        bool ApplicationMain::Input() {
+            auto flag = ApplicationBase::Input();
+            // 入力状態の取得
+            //auto state = _input->GetJoypadState();
+            //// ゲームを終了するかの判定
+            //if (state.GetKey() & PAD_INPUT_1) {
+            //  _state = State::Quit;
+            //  return true;
+            //}
+            return flag;
+        }
+
+        void ApplicationMain::Terminate() {
+            // Effekseerを終了する。
+            Effkseer_End();
+        }
+
+        bool ApplicationMain::Process() {
+            return ApplicationBase::Process();
+        }
+
+        bool ApplicationMain::Effekseer() {
+            //DirectX11を使用
+            SetUseDirect3DVersion(DX_DIRECT3D_11);
+            //Effekseerの初期化
+            //引数には画面に表示する最大パーティクル数を設定
+            if (Effekseer_Init(8000) == -1) {
+                return false;
+            }
+            //フルスクリーンウインドウの切り替えでリソースが消えるのを防ぐ
+            SetChangeScreenModeGraphicsSystemResetFlag(FALSE);
+            //DXライブラリのデバイスロストした時のコールバックを設定
+            Effekseer_SetGraphicsDeviceLostCallbackFunctions();
+            return true;
+        }
+    } // namespace Application
 } // namespace Gyro
--- source/ApplicationMain.h
+++ source/ApplicationMain.h
@@ -1,7 +1,7 @@
 /*****************************************************************//**
  * @file   ApplicationMain.h
  * @brief  アプリケーションベースのサブクラス
- * 
+ *
  * @author 鈴木希海
  * @date   January 2022
  *********************************************************************/
@@ -9,93 +9,121 @@
 #include "appframe.h"
 #include <EffekseerForDXLib.h>
 #include "Camera.h"
+#include "Effect/EffectServer.h"
+#include "Effect/EffectComponent.h"
 
-/**
- * @brief ゲームスペース
- */
+ /**
+  * @brief ゲームスペース
+  */
 namespace Gyro {
-  /**
-   * @brief オブジェクトベース
-   */
-  namespace Object {
-    // 前方宣言
-    class ObjectServer; //!< オブジェクトサーバ
-    class SpawnServer;  //!< スポーンサーバ
-  } // namespace Object
-  /**
-   * @brief アプリケーション
-   */
-  namespace Application {
     /**
-     * @class ApplicationMain
-     * @brief アプリケーションメイン
+     * @brief オブジェクトベース
      */
-    class ApplicationMain : public AppFrame::Application::ApplicationBase {
-    public:
-      /**
-       * @brief  コンストラクタ
-       */
-      ApplicationMain();
-      /**
-       * @brief  初期化
-       * @return true:初期化成功 false:初期化失敗
-       */
-      bool Init() override;
-      /**
-       * @brief  入力処理
-       * @return 
-       */
-      bool Input() override;
-      /**
-       * @brief  終了処理
-       */
-      void Terminate() override;
-      /**
-       * @brief  フレームカウンタの取得
-       * @return フレームカウンタ
-       */
-      const int GetFrameCount() const override {
-        return 0;
-      }
-      /**
-       * @brief  オブジェクトサーバの取得
-       * @return オブジェクトサーバの参照
-       */
-      Object::ObjectServer& GetObjectServer() {
-        return *_objectServer;
-      }
-      /**
-       * @brief  スポーンサーバの取得
-       * @return スポーンサーバの参照
-       */
-      Object::SpawnServer& GetSpawnServer() {
-        return *_spawnServer;
-      }
-      /**
-       * @brief  カメラの取得
-       * @return カメラの参照
-       */
-      Camera::Camera& GetCamera() {
-          return *_camera;
-      }
-    private:
-      //!< オブジェクトサーバ
-      std::unique_ptr<Object::ObjectServer> _objectServer{ nullptr };
-      //!< スポーンサーバ
-      std::unique_ptr<Object::SpawnServer> _spawnServer{nullptr};
-      //!< カメラ
-      std::unique_ptr<Camera::Camera> _camera{ nullptr };
-      /**
-       * @brief  更新処理
-       * @return
-       */
-      bool Process() override;
-      /**
-       * @brief  Effekseer設定
-       * @return true 設定成功
-       *         false 設定失敗
-       */
-      bool Effekseer();
-    };
-  } // namespace Application
+    namespace Object {
+        // 前方宣言
+        class ObjectServer; //!< オブジェクトサーバ
+        class SpawnServer;  //!< スポーンサーバ
+    } // namespace Object
+    /**
+     * @brief エフェクトベース
+     */
+    namespace Effect {
+        // 前方宣言
+        class EffectServer;     //!< エフェクトサーバ
+        class EffectComponent;  //!< エフェクト
+    } // namespace Effect
+    /**
+     * @brief アプリケーション
+     */
+    namespace Application {
+        /**
+         * @class ApplicationMain
+         * @brief アプリケーションメイン
+         */
+        class ApplicationMain : public AppFrame::Application::ApplicationBase {
+        public:
+            /**
+             * @brief  コンストラクタ
+             */
+            ApplicationMain();
+            /**
+             * @brief  初期化
+             * @return true:初期化成功 false:初期化失敗
+             */
+            bool Init() override;
+            /**
+             * @brief  入力処理
+             * @return
+             */
+            bool Input() override;
+            /**
+             * @brief  終了処理
+             */
+            void Terminate() override;
+            /**
+             * @brief  フレームカウンタの取得
+             * @return フレームカウンタ
+             */
+            const int GetFrameCount() const override {
+                return 0;
+            }
+            /**
+             * @brief  オブジェクトサーバの取得
+             * @return オブジェクトサーバの参照
+             */
+            Object::ObjectServer& GetObjectServer() {
+                return *_objectServer;
+            }
+            /**
+             * @brief  スポーンサーバの取得
+             * @return スポーンサーバの参照
+             */
+            Object::SpawnServer& GetSpawnServer() {
+                return *_spawnServer;
+            }
+            /**
+             * @brief  カメラの取得
+             * @return カメラの参照
+             */
+            Camera::Camera& GetCamera() {
+                return *_camera;
+            }
+            /**
+             * @brief  エフェクトサーバの取得
+             * @return エフェクトサーバの参照
+             */
+            Effect::EffectServer& GetEffectServer() {
+                return *_effectServer;
+            }
+            /**
+             * @brief  エフェクトの取得
+             * @return エフェクトの参照
+             */
+            Effect::EffectComponent& GetEffect() {
+                return *_effect;
+            }
+        private:
+            //!< オブジェクトサーバ
+            std::unique_ptr<Object::ObjectServer> _objectServer{ nullptr };
+            //!< スポーンサーバ
+            std::unique_ptr<Object::SpawnServer> _spawnServer{ nullptr };
+            //!< カメラ
+            std::unique_ptr<Camera::Camera> _camera{ nullptr };
+            //!< エフェクトサーバ
+            std::unique_ptr<Effect::EffectServer> _effectServer{ nullptr };
+            //! エフェクト
+            std::unique_ptr<Effect::EffectComponent> _effect{ nullptr };
+            /**
+             * @brief  更新処理
+             * @return
+             */
+            bool Process() override;
+            /**
+             * @brief  Effekseer設定
+             * @return true 設定成功
+             *         false 設定失敗
+             */
+            bool Effekseer();
+        };
+    } // namespace Application
 } // namespace Gyro
